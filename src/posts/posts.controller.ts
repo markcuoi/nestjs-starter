4885ff0a9ddfc90e330c2f7ce4cc12bd9fe2ae4d
Thanh Huu Phan (3073) <thanh.phan@visily.ai>
2023-01-04 11:06:27 +0700

    part2: setting up Postgres with typeorm

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,19 +1,13 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-} from '@nestjs/common';
-import { CreatePostDto } from './dto/createPost.dto';
-import { PostsService } from './posts.service';
-import { UpdatePostDto } from './dto/updatePost.dto';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import PostsService from './posts.service';
+import CreatePostDto from './dto/createPost.dto';
+import UpdatePostDto from './dto/updatePost.dto';
 
 @Controller('posts')
-export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
+export default class PostsController {
+  constructor(
+    private readonly postsService: PostsService
+  ) {}
 
   @Get()
   getAllPosts() {
@@ -30,13 +24,13 @@ export class PostsController {
     return this.postsService.createPost(post);
   }
 
-  @Put(':id')
-  async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
-    return this.postsService.replacePost(Number(id), post);
+  @Patch(':id')
+  async updatePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+    return this.postsService.updatePost(Number(id), post);
   }
 
   @Delete(':id')
   async deletePost(@Param('id') id: string) {
-    this.postsService.deletePost(Number(id));
+    return this.postsService.deletePost(Number(id));
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,50 +1,47 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Post } from './post.interface';
-import { UpdatePostDto } from './dto/updatePost.dto';
+import Post from './post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/createPost.dto';
+import { UpdatePostDto } from './dto/updatePost.dto';
 
 @Injectable()
-export class PostsService {
-  private lastPostId = 0;
-  private posts: Post[] = [];
+export default class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
 
   getAllPosts() {
-    return this.posts;
+    return this.postsRepository.find();
   }
 
-  getPostById(id: number) {
-    const post = this.posts.find((post) => post.id === id);
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne(id);
     if (post) {
       return post;
     }
-
     throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
   }
 
-  replacePost(id: number, post: UpdatePostDto) {
-    const postIndex = this.posts.findIndex((post) => post.id === id);
-    if (postIndex > -1) {
-      this.posts[postIndex] = post;
-      return post;
-    }
-
-    throw new HttpException('Post not fond', HttpStatus.NOT_FOUND);
-  }
-
-  createPost(post: CreatePostDto) {
-    const newPost = {
-      id: ++this.lastPostId,
-      ...post,
-    };
-    this.posts.push(newPost);
+  async createPost(post: CreatePostDto) {
+    const newPost = await this.postsRepository.create(post);
+    await this.postsRepository.save(newPost);
     return newPost;
   }
 
-  deletePost(id: number) {
-    const postIndex = this.posts.findIndex((post) => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    } else {
+  async updatePost(id: number, post: UpdatePostDto) {
+    await this.postsRepository.update(id, post);
+    const updatedPost = await this.postsRepository.findOne(id);
+    if (updatedPost) {
+      return updatedPost;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  async deletePost(id: number) {
+    const deleteResponse = await this.postsRepository.delete(id);
+    if (!deleteResponse.affected) {
       throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
     }
   }
